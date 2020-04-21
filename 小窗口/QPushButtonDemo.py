744d7b8d55d91ce1a5f1e2c0b06f1e89623274c6
gyp0410 <1624329413@qq.com>
2020-04-21 20:44:43 +0800

    MainWindow

--- 小窗口/QPushButtonDemo.py
+++ 小窗口/QPushButtonDemo.py
@@ -44,7 +44,7 @@ class QPushButtonDemo(QDialog):
         #在文本前面显示图像
 
         self.button2 = QPushButton('图像按钮')
-        self.button2.setIcon(QIcon(QPixmap('./images/python.png')))
+        self.button2.setIcon(QIcon(QPixmap('./images/py.ico')))
         self.button2.clicked.connect(lambda:self.whichButton(self.button2))
         layout.addWidget(self.button2)
 
